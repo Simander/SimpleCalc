e44c596a35f124fcf07ee58dcd983a25ec8e8066
Anders Simonsen <anders.stende.simonsen@gmail.com>
2014-06-24 21:34:21 +0200

    Create Funksjoner.java

---
+++ Funksjoner.java
@@ -0,0 +1,32 @@
+//Enkle kalkulatorfunksjoner
+public class Funksjoner
+{
+	Funksjoner()
+	{
+		
+	}
+	//metode for å addere to verdier
+	public float addisjon(float x, float y)
+	{
+		float sum = x + y;
+		return sum;
+	}
+	//metode for å subtrahere to verdier
+	public float subtraksjon(float x, float y)
+	{
+		float sum = x - y;
+		return sum;
+	}
+	//metode for å multiplisere to verdier
+	public float multiplikasjon(float x, float y)
+	{
+		float sum = x * y;
+		return sum;
+	}
+	//metode for å dividere to verdier
+	public float divisjon(float x, float y)
+	{
+		float sum = x / y;
+		return sum;
+	}
+}
